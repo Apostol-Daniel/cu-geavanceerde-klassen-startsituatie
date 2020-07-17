5823d51da6dda4a2df7224b0b5ea7a0931c4ed97
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2020-07-17 18:28:36 +0200

    Create Fiets class and add parameters to it

---
+++ GeavanceerdeKlassen.Lib/Fiets.cs
@@ -0,0 +1,20 @@
+﻿using GeavanceerdeKlassen.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeavanceerdeKlassen.Lib
+{
+    public class Fiets : Voertuig
+    {
+        private bool heeftSpatborden;
+
+        public bool HeeftSpatboorden 
+        {
+            get { return heeftSpatborden; }
+            set { heeftSpatborden = value; }
+        }
+    }
+}
