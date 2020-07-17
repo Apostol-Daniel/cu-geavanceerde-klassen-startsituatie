27a107ad865408f68e2889bac99b70cf9bd34116
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2020-07-17 18:01:21 +0200

    Create Voertuig class and added two properties to it

---
+++ GeavanceerdeKlassen.Wpf/Voertuig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeavanceerdeKlassen.Wpf
+{
+    class Voertuig
+    {
+        private bool heeftEenMotor;
+        private string merk;
+
+        public bool HeeftEenMotor
+        {
+            get { return heeftEenMotor; }
+            set { heeftEenMotor = value; }
+        }
+
+        public string Merk
+        {
+            get { return merk; }
+            set { merk = value; }
+        }
+    }
+}
