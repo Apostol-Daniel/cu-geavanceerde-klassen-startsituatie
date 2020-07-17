40c2ada950a53f902f82e209a54ec2842fd7895e
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2020-07-17 18:03:13 +0200

    Create Auto class and a property yo  it

---
+++ GeavanceerdeKlassen.Wpf/Auto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeavanceerdeKlassen.Wpf
+{
+    class Auto
+    {
+        private int aantalDeuren;
+
+        public int AantalDeuren
+        {
+            get { return aantalDeuren; }
+            set { aantalDeuren = value; }
+        }
+    }
+}
